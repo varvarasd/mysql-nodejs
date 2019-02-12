5ab0015b1b9bd6e9a34aceed189629acc45f5a0e
Daniel Varvara <varvara_sd@yahoo.co.uk>
2019-02-12 21:13:00 +0000

    mysql and nodejs setup with XAMPP

---
+++ app.js
@@ -0,0 +1,44 @@
+
+// Before using the app you must have installed mysql
+// You can download XAMPP from https://www.apachefriends.org/index.html
+// After downloading and installing you must start the Apache server and Mysql then visit http://localhost/phpmyadmin/ for the 
+// mysql dashboard.
+// To run this app you must run 'npm i -g nodemon' then, within the project, run 'npm start'
+
+const express = require('express');
+const mysql = require('mysql');
+
+//CREATE CONNECTION
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root', // this can be edited in mysql dashboard
+    password : 'passwordyousetinmysqlforroot', // to be set in mysql dashboard for this to work
+  });
+
+//CONNECT
+db.connect((err) => {
+    if(err) {
+        throw err;
+    } else {
+        console.log("Connection established. Beam me up, Steve!")
+    }
+});
+
+const app = express();
+
+//CREATE DATABASE
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE nodemysql'; // create a sql query string 
+    db.query(sql, (err, result) => {
+        if(err){
+            throw err;
+        } else {
+            console.log(result);
+            res.send('Reason: complaint! Subject: Steve!');
+        }
+    });
+});
+
+app.listen( '3000', () => {
+    console.log('app server started on port 3000');
+});
